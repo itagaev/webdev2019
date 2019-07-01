e4b74c7dbe95a5f30419ca80e15c5c75f1170a02
tagaev123 <tagayev.ilias@gmail.com>
2019-07-01 21:32:31 +0600

    first commit

---
+++ mazebot0/mazegame/GameSession.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using pp2.lecture8.mazegame.gameobjects;
+
+namespace pp2.lecture8.mazegame
+{
+    public class GameSession
+    {
+        private Wall wall;
+        private Exit exit;
+        private Person person { get; set; }
+        private int level = 1;
+
+        public int getLevel()
+        {
+            return this.level;
+        }
+        public void setLevel(int level)
+        {
+            this.level = level;
+        }
+
+        public GameSession()
+        {
+            wall = new Wall('#');
+            exit = new Exit('X');
+            person = new Person('P');
+        
+            LoadMap(level);
+        }
+        public Exit getExit()
+        {
+            return this.exit;
+        }
+        public Person getPerson()
+        {
+            return this.person;
+        }
+
+        public Wall getWall()
+        {
+            return this.wall;
+        }
+
+        public GameState play(GameAction action)
+        {
+            render();
+
+            return GameState.PLAYING;
+        }
+
+        private void render()
+        {
+            wall.Clear();
+            exit.Clear();
+            person.Clear();
+
+            wall.Draw();
+            exit.Draw();
+            person.Draw();
+        }
+
+        public void LoadMap(int level)
+        {
+            Console.Clear();
+            string filePath = string.Format("Levels/Level{0}.txt", level);
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            bool isPersonLoaded = false;
+            bool isExitLoaded = false;
+            int x = 0, y = 0;
+            person.ClearLocations();
+            exit.ClearLocations();
+            wall.ClearLocations();
+            Console.SetCursorPosition(50, 0);
+            Console.WriteLine("Your Level: " + this.level);
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                for (x=0;x<line.Length;x++)
+                {
+                    if (line[x] == wall.GetLebel())
+                    {
+                        wall.AddPoint(new Point(x, y));
+                    } else if (line[x] == person.GetLebel() && !isPersonLoaded)
+                    {
+                       person.AddPoint(new Point(x, y));
+                       //person.PrependPoint(new Point(x, y));
+                       isPersonLoaded = true;
+                    } else if (line[x] == exit.GetLebel() && !isExitLoaded)
+                    {
+                        exit.AddPoint(new Point(x, y));
+                        isExitLoaded = true;
+                    }
+                }
+
+                y++;
+            }
+
+            sr.Close();
+            fs.Close();
+
+
+        }
+
+    }
+}
---
+++ mazebot0/mazegame/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using pp2.lecture8.mazegame.gameobjects;
+namespace pp2.lecture8.mazegame
+{
+
+    public enum GameState
+    {
+        START, PLAYING, WIN, LOST, PAUSE
+    }
+
+    public enum GameAction
+    {
+        START, MOVE_UP, MOVE_DOWN, MOVE_RIGHT, MOVE_LEFT, PAUSE, UNKNOWN
+    }
+
+    public class Program
+    {
+        static GameAction KeyToGameAction(ConsoleKeyInfo keyInfo)
+        {
+
+            ConsoleKey key = keyInfo.Key;
+            switch (key)
+            {
+                case ConsoleKey.UpArrow: return GameAction.MOVE_UP;
+                case ConsoleKey.DownArrow: return GameAction.MOVE_DOWN;
+                case ConsoleKey.LeftArrow: return GameAction.MOVE_LEFT;
+                case ConsoleKey.RightArrow: return GameAction.MOVE_RIGHT;
+                case ConsoleKey.Escape: return GameAction.PAUSE;
+                case ConsoleKey.Enter: return GameAction.START;
+                default: return GameAction.UNKNOWN;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            //1.To Load the map by level
+            //2.To move person by this map
+            //3.When person finds exit new map must be loaded
+
+            GameState state = GameState.START;
+            GameSession session = new GameSession();
+            GameAction action = GameAction.UNKNOWN;
+            state = session.play(action);
+            int currentLevel = 1;
+            int maxLevel = 3;
+
+            //game loop
+            while (state != GameState.LOST)
+            {
+                Person person = session.getPerson();
+                Wall currentWall = session.getWall();
+                Exit currentExit = session.getExit();
+                
+                action = KeyToGameAction(Console.ReadKey());
+
+                if (action == GameAction.MOVE_UP)
+                    person.move(0, -1);
+                if (action == GameAction.MOVE_DOWN)
+                    person.move(0, 1);
+                if (action == GameAction.MOVE_LEFT)
+                    person.move(-1, 0);
+                if (action == GameAction.MOVE_RIGHT)
+                    person.move(1, 0);
+
+                state = session.play(action);
+
+                if (person.IsWallCollision(currentWall))
+                {
+                    Console.Clear();
+                    Console.SetCursorPosition(50, 10);
+                    Console.WriteLine("YOU ARE LOST!!!");
+                    Console.ReadKey();
+                    Console.Clear();
+                    currentLevel = 1;
+                    session = new GameSession();
+                    session.LoadMap(currentLevel);
+                    state = session.play(action);
+                }
+                if (person.getHead().X == currentExit.getPosition().X && person.getHead().Y == currentExit.getPosition().Y)
+                {
+                    Console.Clear();
+                    currentLevel++;
+                    if(currentLevel == maxLevel + 1)
+                    {
+                        Console.Clear();
+                        Console.SetCursorPosition(50, 10);
+                        Console.WriteLine("WIN!!!");
+                        Console.ReadKey();
+                        Console.Clear();
+                        currentLevel = 1;
+                    }
+                    session = new GameSession();
+                    person.ClearLocations();
+                    session.setLevel(currentLevel);
+                    session.LoadMap(currentLevel);
+                    state = session.play(action);
+                }
+            }
+            
+        }
+    }
+}
